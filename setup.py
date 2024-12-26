54afa7edba5bd9dd4405e2bae4f7e548ebf46fac
unknown <panchariyap1@gmail.com>
2024-12-26 11:05:01 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Information-Retrieval-System",
+    version = "0.0.0",
+    author="Piyush",
+    author_email="panchariyap1@gmail.com",
+    packages=find_packages(),
+    install_requires = []
+)
